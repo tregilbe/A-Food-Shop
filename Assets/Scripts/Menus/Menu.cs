83146d888e636df5e33fd84f2d6ccf8f13bf8d13
Trenton <tregilbe@uat.edu>
2023-08-20 12:51:08 -0500

    Main Menu added
    Made a basic Main Menu with Start, Quit, and a How to Play which needs the image but its there for now. Added main menu and level one to the scene list in order.

---
+++ Assets/Scripts/Menus/Menu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void OnPlayButton()
+    {
+        // Loads the second scene in the index manager
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnQuitButton()
+    {
+        Application.Quit();
+    }
+}
