3f7c825c1766016ec41ff126566e3e44b7001a60
Trenton <tregilbe@uat.edu>
2023-08-20 22:28:36 -0500

    Made prefabs fam
    please

--- Assets/Scripts/Interaction System/BasementHatch.cs
+++ Assets/Scripts/Interaction System/BasementHatch.cs
@@ -10,7 +10,7 @@ public class BasementHatch : MonoBehaviour, I_Interactable
     [SerializeField] private Transform TargetEntryPoint;
     [SerializeField] private Transform ItemTargetPoint;
 
-    public GameObject player;
+    //public GameObject player;
 
     private void Start()
     {
@@ -24,13 +24,13 @@ public class BasementHatch : MonoBehaviour, I_Interactable
         Vector3 target = new Vector3 (TargetEntryPoint.position.x, TargetEntryPoint.position.y, TargetEntryPoint.position.z);
 
         // disable player controller to teleport
-        player.GetComponent<CharacterController>().enabled = false;
+        //player.GetComponent<CharacterController>().enabled = false;
 
         // teleport player to the area
-        player.GetComponent<Transform>().position = target;
+        //player.GetComponent<Transform>().position = target;
 
         // re enable the character controller
-        player.GetComponent<CharacterController>().enabled = true;
+        //player.GetComponent<CharacterController>().enabled = true;
 
         Debug.Log("Tried to move player to basement");
         return true;
