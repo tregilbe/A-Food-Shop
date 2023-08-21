daad6318a079c54e093ac7847fd73d642d9a4d28
Ghostninja2451 <80051664+Ghostninja2451@users.noreply.github.com>
2023-08-20 22:50:24 -0600

    AI eating certain good

--- Assets/Scripts/AI/EatingFood.cs
+++ Assets/Scripts/AI/EatingFood.cs
@@ -6,29 +6,52 @@ using UnityEngine.Assertions;
 public class EatingFood : MonoBehaviour
 {
     [SerializeField] AINavigation ai;
-    
-
+    SpawningSystem spawnsystem;
+    public GameObject points;
+    public GameObject miniMe;
+    public Transform spawner;
     public GameObject poisonFood;
+    
     private ObjectGrabbable poison;
     private void Awake()
     {
         poison = FindObjectOfType<ObjectGrabbable>();
+        spawnsystem = FindObjectOfType<SpawningSystem>();
         Assert.IsNotNull(poison);
     }
 
     private void Start()
     {
         poison = poisonFood.GetComponent<ObjectGrabbable>();
-
+        spawnsystem = points.GetComponent<SpawningSystem>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Poison")
         {
-            print("Dead!!!!");
+            //print("Dead!!!!");
+            Destroy(this.gameObject);
+            spawnsystem.spawnCount--;
+            Instantiate(miniMe, spawner.position, spawner.rotation);
             ai.EnableRagDoll();
-            Destroy(GameObject.FindWithTag("Poison"));
+            if (transform.parent != null)
+            {
+                Destroy(transform.parent.gameObject, 2);
+            }
+            Destroy(this.gameObject, 1);
+        }
+        else if(other.gameObject.tag == "Food")
+        {
+            //print("Yummy");
+            Destroy(other.gameObject);
+            spawnsystem.spawnCount--;
+            if (transform.parent != null)
+            {
+                Destroy(transform.parent.gameObject);
+            }
+            Destroy(this.gameObject);
+
         }
     }
 }
