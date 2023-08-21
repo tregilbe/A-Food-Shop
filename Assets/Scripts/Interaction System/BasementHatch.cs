cbd35293cd5e9154691fe330e1adce1cf61ac806
Trenton <tregilbe@uat.edu>
2023-08-20 22:45:09 -0500

    Reapplied all missing scripts
    Resetup main level, reimplemented scripts. Level 1 is now back to normal

--- Assets/Scripts/Interaction System/BasementHatch.cs
+++ Assets/Scripts/Interaction System/BasementHatch.cs
@@ -8,30 +8,39 @@ public class BasementHatch : MonoBehaviour, I_Interactable
     [SerializeField] private string prompt;
 
     [SerializeField] private Transform TargetEntryPoint;
+    [SerializeField] private Transform ItemTargetPoint;
 
-    //public GameObject player;
+    public GameObject player;
 
     private void Start()
     {
-        
+
     }
 
     public string InteractionPrompt => prompt;
     public bool Interact(Interactor interactor)
     {
         // make a vector 3 out of target transform
-        Vector3 target = new Vector3 (TargetEntryPoint.position.x, TargetEntryPoint.position.y, TargetEntryPoint.position.z);
+        Vector3 target = new Vector3(TargetEntryPoint.position.x, TargetEntryPoint.position.y, TargetEntryPoint.position.z);
 
         // disable player controller to teleport
-        //player.GetComponent<CharacterController>().enabled = false;
+        player.GetComponent<CharacterController>().enabled = false;
 
         // teleport player to the area
-        //player.GetComponent<Transform>().position = target;
+        player.GetComponent<Transform>().position = target;
 
         // re enable the character controller
-        //player.GetComponent<CharacterController>().enabled = true;
+        player.GetComponent<CharacterController>().enabled = true;
 
         Debug.Log("Tried to move player to basement");
         return true;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player")
+        {
+            other.transform.position = ItemTargetPoint.position;
+        }
     }
 }
